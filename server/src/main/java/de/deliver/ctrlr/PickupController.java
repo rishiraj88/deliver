da953e18392043cb086799dadd680538d02e2426
Rishi Raj <rishiraj88+github@gmail.com>
2024-03-04 23:12:35 +0100

    Fix the annotations on PickupController class.

--- server/src/main/java/de/deliver/ctrlr/PickupController.java
+++ server/src/main/java/de/deliver/ctrlr/PickupController.java
@@ -1,9 +1,11 @@
 package de.deliver.ctrlr;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api/v1_0")
+@RestController()
+@RequestMapping("/api/v1_0")
 public class PickupController {
     @GetMapping("/pickup")
     public String doPickup() {
